21ed22095a60cbe7b7846a1d2b01c6a5eba7c9b9
Dmitry Yevlakhov <tragenstolz@gmail.com>
2019-07-06 23:59:56 +0300

    alpha

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,6 @@ module.exports = {
 
     "no-shadow": ["error", { "builtinGlobals": false }],
     "react/destructuring-assignment": 0,
+    "react/jsx-one-expression-per-line": 0,
   }
 };
--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
-const App = () => {
-  return (
+const Home = () => <div>Home page</div>;
+const Cxc = () => <div>Sanya hui sosi</div>;
+const NoMatch = () => <div>Not found</div>
+
+const App = () => (
+  <Router>
     <div>
-      
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/cxc">CXC page</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/cxc" component={Cxc} />
+        <Route component={NoMatch} />
+      </Switch>
     </div>
-  )
-}
+  </Router>
+);
 
-export default App
+export default App;
