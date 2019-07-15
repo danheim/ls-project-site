cc14a7eb9340d523c22e8008f345b2172e4be5d0
Dmitry Yevlakhov <tragenstolz@gmail.com>
2019-07-15 20:15:39 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 import React from 'react';
-import Content from './components/Content';
+import { Switch, Route, Link } from 'react-router-dom';
 
-const App = () => <Content />;
+const App = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/products">Products</Link>
+        </li>
+        <li>
+          <Link to="/afdfad">No Match</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/products" component={Products} />
+        <Route component={NoMatch} />
+      </Switch>
+    </div>
+  );
+};
+
+const Home = () => <h2>home</h2>;
+const Products = () => <h2>Products</h2>;
+const NoMatch = () => <h2>No match</h2>;
 
 export default App;
